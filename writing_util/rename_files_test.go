3c75489563a03874c21f082778ede9e277156b1c
caijiatao <caijiatao@sics.ac.cn>
2024-02-19 17:36:22 +0800

    feature :add task manager

--- writing_util/rename_files_test.go
+++ writing_util/rename_files_test.go
@@ -6,6 +6,6 @@ import (
 )
 
 func Test_removeSpaceFilesInFolder(t *testing.T) {
-	err := removeSpaceFilesInFolder(`C:\Users\caijiatao\Downloads\da2937ab-94a5-4190-8192-637145ab608f_Export-4ccef35a-ad18-4ef6-af75-f8c62d1de63c\a7cec09e91b34db4b501776888c99b8a`, ".png")
+	err := removeSpaceFilesInFolder(`C:\Users\caijiatao\Downloads\bc100d51-f259-4c3d-9cf1-662eccc1e4a6_Export-31ee2d10-bf51-4a8b-9e42-d99ad94de840\9b49f76db38d43f3a329b63c1d4d51d9`, ".png")
 	assert.Nil(t, err)
 }

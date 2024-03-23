78b7e3e7312fba954861854e20241123d66e598d
karynchen <karynchen@tencent.com>
2024-03-23 12:18:59 +0800

    feature: add logic

---
+++ system_solution/chat/chat-app-svr/rpc/user/internal/logic/authlogic.go
@@ -0,0 +1,30 @@
+package logic
+
+import (
+	"context"
+
+	"chat-app-svr/rpc/user/internal/svc"
+	"chat-app-svr/rpc/user/user"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type AuthLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewAuthLogic(ctx context.Context, svcCtx *svc.ServiceContext) *AuthLogic {
+	return &AuthLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *AuthLogic) Auth(in *user.AuthRequest) (*user.AuthReply, error) {
+	// todo: add your logic here and delete this line
+
+	return &user.AuthReply{}, nil
+}

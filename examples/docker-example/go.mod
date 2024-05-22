bf19aed9bec564521c66939e2238e46fd3c2dcdb
caijiatao <caijiatao@sics.ac.cn>
2024-05-22 10:44:23 +0800

    feature :add util

--- examples/docker-example/go.mod
+++ examples/docker-example/go.mod
@@ -1,5 +1,3 @@
 module docker-example
 
 go 1.19
-
-require gitlab.grandhoo.com/rock/rock-share v1.6.109
--- examples/docker-example/main.go
+++ examples/docker-example/main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"fmt"
-	"gitlab.grandhoo.com/rock/rock-share/global/errors"
 )
 
 func main() {
-	fmt.Println(errors.NAME_NOT_ALLOW_NULL.Code)
+	fmt.Println(1)
 }
